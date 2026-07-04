6921e998d6963bceabc7f288aa8c387bbb546fa8
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2026-07-04 22:29:50 +0530

    redis.js updated

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,16 +1,12 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
+const redis = new Redis(process.env.REDIS_URL || {
   host: "127.0.0.1",
   port: 6379,
 });
 
-redis.on("connect", () =>
-  console.log("✅ Redis connected")
-);
+redis.on("connect", () => console.log("✅ Redis connected"));
 
-redis.on("error", (err) =>
-  console.log("❌ Redis error", err)
-);
+redis.on("error", (err) => console.log("❌ Redis error", err));
 
 module.exports = redis;
